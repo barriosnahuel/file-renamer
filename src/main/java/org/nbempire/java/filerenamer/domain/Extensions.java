59b35792f7f9e6985533d5661cdfd55c966b811b
barriosnahuel <barrios.nahuel@gmail.com>
2012-08-17 20:28:24 -0300

    Add txt, jpg, jpeg, png extensions to valid file extensions to rename.

--- src/main/java/org/nbempire/java/filerenamer/domain/Extensions.java
+++ src/main/java/org/nbempire/java/filerenamer/domain/Extensions.java
@@ -1,12 +1,14 @@
 package org.nbempire.java.filerenamer.domain;
 
 /**
- * TODO : JavaDoc : for Extensions.
+ * Available extensions that this version of the API is able to rename.
+ * <p/>
+ * This <code>enum</code> will be deprecated as soon as possible.
  *
- * @author Thales - PNT Equipo 6.
+ * @author Nahuel Barrios.
  */
 public enum Extensions {
 
-    mp3
+    mp3, txt, jpg, jpeg, png
 
 }
